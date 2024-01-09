84a37d47d3daf7882958afc1e3d39620eed7c28f
Pratham_Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2024-01-09 23:00:00 +0530

    fix a bug and add functionality

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,12 +1,13 @@
 use std::io; 
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Let's get start number guessing game"); 
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    print("The secret number is: {secret_number}");
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess: ");
 
@@ -15,6 +16,20 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line"); 
+
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num, 
+        Err(_) => {
+            println!("Please enter a valid number!"); 
+            return;
+        }
+    };
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 
     println!("you guessed: {guess}");
 
