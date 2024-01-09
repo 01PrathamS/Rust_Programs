f89fb52e3ed699ecfea2421e587e2ed057fcf750
Pratham_Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2024-01-09 22:50:41 +0530

    update code with add secret_number functionality

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,7 +1,12 @@
 use std::io; 
+use rand::Rng;
 
 fn main() {
     println!("Let's get start number guessing game"); 
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    print("The secret number is: {secret_number}");
 
     println!("Please input your guess: ");
 
